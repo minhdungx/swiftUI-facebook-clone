f2f51b79435e6e30398cd195316af402b9134ea0
DungHM <dunghm@rikkeisoft.com>
2025-05-21 09:38:20 +0700

    Initial Commit

---
+++ facebook-swiftUI/facebook_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  facebook_swiftUIApp.swift
+//  facebook-swiftUI
+//
+//  Created by DungHM on 21/5/25.
+//
+
+import SwiftUI
+
+@main
+struct facebook_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
